462c071bb309979217eafa3643171359f390235e
Vipul Gupta <vipul31gupta@gmail.com>
2025-08-03 00:16:47 +0530

    Update checkbox.test.ts

--- tests/checkbox.test.ts
+++ tests/checkbox.test.ts
@@ -2,15 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('Checkbox demo page loads', async ({ page }) => {
  await page.goto('https://www.lambdatest.com/selenium-playground/');
- await page.getByText('Checkbox Demo').click;
- await page.getByText('Single Checkbox Demo').isVisible;
+ await page.getByText('Checkbox Demo').click();
+ await expect(page.getByText('Single Checkbox Demo')).toBeVisible();
 });
 
 test('Checkbox checked on click', async ({ page }) => {
  await page.goto('https://www.lambdatest.com/selenium-playground/');
- await page.getByText('Checkbox Demo').click;
- await page.getByText('Single Checkbox Demo').isVisible;
- const checkbox = await page.locator('type=checkbox');
- await checkbox.click;
- await page.getByText('Checked!').isVisible;
+ await page.getByText('Checkbox Demo').click();
+ const checkbox = await page.locator("input[type='checkbox']");
+ await checkbox.nth(0).click();
+ await expect(page.getByText('Checked!')).toBeVisible();
 });
